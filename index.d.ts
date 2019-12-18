aa9a0ffee7466bcd72d8bfef668ec521f79c3c2b
Ilya Istomin <procot@yandex-team.ru>
2019-12-19 00:22:48 +0300

    reexport typings from types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,16 @@
 import { WebOS } from './types/WebOS';
 import { WebOSDev } from './types/WebOSDev';
 
+export * from './types/ConnectionStatus';
+export * from './types/DRMAgent';
+export * from './types/DeviceInfo';
+export * from './types/LGUDIDResponse';
+export * from './types/LaunchParameters';
+export * from './types/ServiceRequest';
+export * from './types/SystemInfo';
+export * from './types/WebOS';
+export * from './types/WebOSDev';
+export * from './types/drm';
+
 declare const webOS: WebOS;
 declare const webOSDev: WebOSDev;
